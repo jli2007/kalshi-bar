55f5b7a53e8211025892da0f16996801dd7cb0f2
Austin Jian <austin.jian07@gmail.com>
2026-02-14 23:33:07 -0500

    ui: add magic ui shine border

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   return (
     <div className="flex h-screen flex-col">
       <Navbar />
-      
+      <div className="absolute left-4 top-20 z-10 w-80">
+        <BarCard bar={bars[2]} />
+      </div>
       <main className="flex-1">
         <Map />
       </main>
--- frontend/src/components/BarCard.tsx
+++ frontend/src/components/BarCard.tsx
@@ -1,20 +1,23 @@
 import { ExternalLinkIcon } from "@radix-ui/react-icons";
 import type { Bar } from "@/data/bars";
+import { ShineBorder } from "@/components/ui/ShineBorder";
 
 export default function BarCard({ bar }: { bar: Bar }) {
-  const mapImgUrl = `https://api.mapbox.com/styles/v1/mapbox/dark-v11/static/${bar.coordinates[0]},${bar.coordinates[1]},14,0/400x200@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`;
-
   return (
-    <div className="overflow-hidden rounded-xl border border-white/15 bg-kalshi-card shadow-[0_0_12px_rgba(255,255,255,0.04)]">
-      {/* Map thumbnail */}
-      <div className="relative">
-        <img
-          src={mapImgUrl}
-          alt={`Map of ${bar.name}`}
-          className="h-32 w-full object-cover"
-        />
-        <div className="absolute inset-x-0 bottom-0 h-8 bg-gradient-to-t from-kalshi-card to-transparent" />
-      </div>
+    <div className="relative rounded-xl bg-kalshi-card">
+      <ShineBorder shineColor="#28CC95" borderWidth={1} duration={10} className="z-10 opacity-70" />
+
+      {/* Bar image */}
+      {bar.image && (
+        <div className="relative overflow-hidden rounded-t-xl">
+          <img
+            src={bar.image}
+            alt={bar.name}
+            className="h-32 w-full object-cover"
+          />
+          <div className="absolute inset-x-0 bottom-0 h-8 bg-gradient-to-t from-kalshi-card to-transparent" />
+        </div>
+      )}
 
       <div className="p-4">
         {/* Header row: name + external link */}
